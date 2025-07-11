32b1cb6e4a2dbf68023cb026ce0657965dfd352d
marekryb <60657309+marekryb@users.noreply.github.com>
2025-07-11 10:56:40 +0900

    changes

--- script.ts
+++ script.ts
@@ -1,35 +1,37 @@
-import { PrismaClient } from '@prisma/client';
-import { enhance } from '@zenstackhq/runtime';
+import { PrismaClient } from "./.prisma/client";
+import { enhance } from "./.zenstack/enhance";
 
-const prisma = new PrismaClient().$extends({
-    model: {
-        user: {
-            async signUp(email: string) {
-                return prisma.user.create({ data: { email } });
-            },
-        },
+const prisma = new PrismaClient();
+const prismaExtended = prisma.$extends({
+  model: {
+    user: {
+      async signUp(email: string) {
+        return prisma.user.create({ data: { email } });
+      },
     },
+  },
 });
 
 const db = enhance(prisma);
+const dbExtended = enhance(prismaExtended);
 
 async function main() {
-    const user = await db.user.create({
-        data: {
-            name: 'John Doe',
-            email: 'a@b.com',
-        },
-    });
-    console.log(user.email);
-    const post = await db.post.create({
-        data: {
-            title: 'Hello World',
-        },
-    });
-    console.log(post.title);
+  const user = await db.user.create({
+    data: {
+      name: "John Doe",
+      email: "a@b.com",
+    },
+  });
+  console.log(user.email);
+  const post = await db.post.create({
+    data: {
+      title: "Hello World",
+    },
+  });
+  console.log(post.title);
 
-    const newUser = await db.user.signUp('a@b.com');
-    console.log(newUser.email);
+  const newUser = await dbExtended.user.signUp("a@b.com");
+  console.log(newUser.email);
 }
 
 main();
